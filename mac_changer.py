2e956cb5b2fe5d24df9290c433eabe05b6323332
Danish Davis <danishdavis@hotmail.com>
2020-02-16 20:01:10 -0500

    MAC address changer

---
+++ mac_changer.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+import subprocess
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="mac", help="New MAC address")
+    (options, arguments) =  parser.parse_args()
+    if not options.interface:
+        parser.error(("[-] Please specify an interface, use --help for more info"))
+    elif not options.mac:
+        parser.error(("[-] Please specify an mac address, use --help for more info"))
+    return options
+
+def change_mac(interfaceparm, new_macparm):
+    print("[+]Changing MAC address for " + interfaceparm + " to " + new_macparm)
+    subprocess.call(["ifconfig", interfaceparm, "down"])
+    subprocess.call(["ifconfig", interfaceparm, "hw", "ether", new_macparm])
+    subprocess.call(["ifconfig", interfaceparm, "up"])
+
+
+options= get_arguments()
+change_mac(options.interface, options.mac)
