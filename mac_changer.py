0d79deda72aebfbdb75aaf7837f5eb11277cdbfa
Danish Davis <danishdavis@hotmail.com>
2020-02-17 13:17:56 -0500

    error checks

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
 import subprocess
 import optparse
+import re
 # ffdgdg
 
 def get_arguments():
@@ -20,6 +21,22 @@ def change_mac(interfaceparm, new_macparm):
     subprocess.call(["ifconfig", interfaceparm, "hw", "ether", new_macparm])
     subprocess.call(["ifconfig", interfaceparm, "up"])
 
+def get_mac_address(interfaceparm):
+    ifconfig_result = subprocess.check_output(["ifconfig", interfaceparm])
+    mac_regex = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_regex:
+        return mac_regex.group(0)
+    else:
+        print("Could not find a MAC address")
+
 
 options= get_arguments()
+current_mac = get_mac_address(options.interface)
+print("Current MAC : "+ str(current_mac))
 change_mac(options.interface, options.mac)
+current_mac = get_mac_address(options.interface)
+if current_mac == options.mac:
+    print("[+] MAC address was successfully changed to "+ current_mac)
+else:
+    print("[-] MAC address did not get changed")
